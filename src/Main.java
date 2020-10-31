1b046d8c0c935bb11876ad1b7335ed68dc9f7830
Tomi <pintertamas99@gmail.com>
2020-10-31 18:23:21 +0100

    i have a green background with a differently filled rectangles on it.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.canvas.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    private final int menuHeight = 100;
+    private final int gameWindowSize = 800;
+
+    private GraphicsContext gc;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,24 +18,39 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        try{
+        try {
             primaryStage.setTitle("Snake");
             primaryStage.setResizable(false);
-
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root, 400, 400);
-            scene.getStylesheets().add(getClass().getResource("game.css").toExternalForm());
-
-            Tail tail = new Tail(10,10,20,20);
-            Snake snek = new Snake(5, tail);
-
+            Group root = new Group();
+            Canvas canvas = new Canvas(gameWindowSize, gameWindowSize);
+            root.getChildren().add(canvas);
+            Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
-
-        }
-        catch(Exception ex) {
+            gc = canvas.getGraphicsContext2D();
+            run();
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
+    }
+
+    private void run() {
+        drawBackground(gc);
+    }
+
+    private void drawBackground(GraphicsContext gc) {
+        int cellCount = 20;
+        for (int i = 0; i < cellCount; i++) {
+            for (int j = 0; j < cellCount; j++) {
+                if ((i + j) % 2 == 0)
+                    gc.setFill(Color.web("AAD751"));
+                else {
+                    gc.setFill(Color.web("A2D149"));
+                }
+                int cellSize = gameWindowSize / cellCount;
+                gc.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+            }
+        }
     }
 }
