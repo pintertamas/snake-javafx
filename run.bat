919871489eece50f40cce483904753ec306214f2
Tomi <pintertamas99@gmail.com>
2020-11-27 13:01:42 +0100

    edited the batch files so the javafx path gets updated to the one im using also added the javafx library to the fxlib directory

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+set current_dir=%cd%\fxlib\javafx-sdk-11.0.2\lib
+echo The JavaFX directory is this: %current_dir%
 cd output
-java --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules javafx.controls Main
+java --module-path %current_dir% --add-modules javafx.controls Main
 cd ..
--- setup.bat
+++ setup.bat
@@ -1,8 +1,10 @@
 @echo off
+set current_dir=%cd%\fxlib\javafx-sdk-11.0.2\lib
+echo The JavaFX directory is this: %current_dir%
 mkdir output
 copy src\*.java output\
 copy src\*.css output\
 cd output
-javac --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules javafx.controls Main.java
+javac --module-path %current_dir% --add-modules javafx.controls Main.java
 del *.java
 cd ..
